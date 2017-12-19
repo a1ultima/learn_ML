636029d242b9c1a559bc7ba72271b640f9f59f1e
a1ultima <a1ultima@gmail.com>
2017-12-19 19:00:59 +0000

    Added Eclat (Association Rule Learning), before testing

--- 5_Association_Rule_Learning/Association_andy/Apriori/apriori.py
+++ 5_Association_Rule_Learning/Association_andy/Apriori/apriori.py
@@ -5,17 +5,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-#
-
 # Data Preprocessing
 dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
-transactions = []
-for i in range(0, 7501):
-    transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
 
-# Training Apriori on the dataset
+# Generating correct input format
+transactions = [[str(dataset.values[i,j]) for i in range(0,7501)] for j in range(0,20)]
+
+# Running the Apriori algorithm
 from apyori import apriori
-rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
 
-# Visualising the results
-results = list(rules)
+results = apriori(transactions, min_support = 0.003, min_lift = 4, min_confidence = 0.2, min_length = 2)
+
+## Intuition:
+# - Support(M) = #transactions inc. M / #Total transactions, i.e. M's popularity
+# - Confidence(M->N) = #Transactions with rule M->N / #transactions inc. (M)
+# - Lift(M->N) = Condifence(M->N) / support(M), i.e. how many times more M->N is observed vs. random, >1 the better
+
+# - we decide Support(M) min to be 0.003 since 3*7/7501 is an item that is bought >3 times per day (21 times per week)
+# - we decide min confidence using trial-and-error, we start w/ depreciating value until we have a workable #rules output
+# - support >4 means a rule that is 4 or more times more relevant than random (1)
+
+results = list(results) # click on the relevant fields to find the embedded stats for each rule
